3c654c6f6a6c89a774b14bd561db3f2282c7a463
David Torres <davidthekoa@gmail.com>
2021-11-04 23:37:26 -0500

    Adding input for size of the square

--- Cpp_Challenges/square.cpp
+++ Cpp_Challenges/square.cpp
@@ -5,15 +5,18 @@ using namespace std;
 
 int main(){
 
-    int a, i, size = 20;
+    int a, i, size;
+
+    cout << "Give the size of the square:\n";
+    cin >> size;
 
     for (i = 1; i <= size; i++){
-        for (a = 1; a <= size + size; a++){
+        for (a = 1; a <= size + size - 2; a++){
             if (i == 1 || i == size)
                 cout << "8";
-            else if (a == 1 || a == size + size)
+            else if (a == 1 || a == size + size - 2)
                 cout << "8";
-            else if (a > 1 && a < size + size)
+            else if (a > 1 && a < size + size - 2)
                 cout << " ";
         }
         cout << endl;
