d07b78048504615db19e93529e9e27754e5549dd
David Torres <davidthekoa@gmail.com>
2021-11-10 20:48:35 -0500

    :tada: First part of the square with pattern

---
+++ Cpp_Challenges/Concentric_square_pattern.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Program to print a concentric square pattern from 1 to 9.
+
+int main(){
+
+    int a, b, last, size;
+
+    cout << "Write the size of the pattern square from 1 to 9:\n";
+    cin >> size;
+    cout << "\n";
+
+    for(a = 1; a <= size; a++){
+        for(b = 1; b <= size; b++){
+            if(a < b)
+                last = a;
+            else
+                last = b;
+            cout << size - last + 1 << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
